eadf4f5781241f233c21736f1720ba7452075b40
Omega-Dynasty <jeremyhugues59@gmail.com>
2026-04-24 17:07:24 +0200

    Fix: Implementation des corps de fonctions dans fs.c

--- fs.h
+++ fs.h
@@ -7,15 +7,14 @@
 
 #define MAX_NAME 50
 #define MAX_CONTENT 1024
+#define DISK_SIZE 5000 
 
-// Structure pour un fichier
 typedef struct File {
     char name[MAX_NAME];
     char content[MAX_CONTENT];
     struct File* next;
 } File;
 
-// Structure pour un répertoire
 typedef struct Directory {
     char name[MAX_NAME];
     struct Directory* parent;
@@ -24,10 +23,14 @@ typedef struct Directory {
     File* files;
 } Directory;
 
-// Prototypes
+// Prototypes indispensables pour le compilateur
 Directory* create_directory(const char* name, Directory* parent);
 void create_file(Directory* dir, const char* name, const char* content);
 void list_content(Directory* dir);
 Directory* change_directory(Directory* current, const char* name);
+void read_file(Directory* dir, const char* name);         // Ajouté
+void write_to_file(Directory* dir, const char* name, const char* new_content);
+void delete_file(Directory* dir, const char* name);       // Ajouté
+void check_space(Directory* root);                        // Ajouté
 
 #endif
--- main.c
+++ main.c
@@ -3,19 +3,43 @@
 int main() {
     Directory* root = create_directory("root", NULL);
     Directory* current = root;
+    char command[20], arg1[MAX_NAME], arg2[MAX_CONTENT];
 
-    // Création de base
-    create_directory("Documents", current);
-    create_directory("Images", current);
-    create_file(current, "info.txt", "Systeme initialise.");
+    printf("--- Bienvenue dans votre Gestionnaire de Fichiers C ---\n");
+    printf("Commandes : ls, mkdir <nom>, touch <nom> <contenu>, cd <nom>, read <nom>, rm <nom>, space, exit\n");
 
-    list_content(current);
+    while (1) {
+        printf("\n%s > ", current->name); // Affiche le prompt interactif
+        if (scanf("%s", command) == EOF) break;
 
-    // Test de navigation
-    printf("\nEntree dans 'Documents'...\n");
-    current = change_directory(current, "Documents");
-    create_file(current, "secret.txt", "Ceci est dans Documents");
-    list_content(current);
-
+        if (strcmp(command, "ls") == 0) {
+            list_content(current);
+        } else if (strcmp(command, "mkdir") == 0) {
+            scanf("%s", arg1);
+            create_directory(arg1, current);
+        } else if (strcmp(command, "touch") == 0) {
+            scanf("%s %s", arg1, arg2);
+            create_file(current, arg1, arg2);
+        } else if (strcmp(command, "cd") == 0) {
+            scanf("%s", arg1);
+            current = change_directory(current, arg1);
+        } else if (strcmp(command, "read") == 0) {
+            scanf("%s", arg1);
+            read_file(current, arg1);
+        } else if (strcmp(command, "rm") == 0) {
+            scanf("%s", arg1);
+            delete_file(current, arg1);
+        } else if (strcmp(command, "space") == 0) {
+            check_space(current);
+        } else if (strcmp(command, "exit") == 0) {
+            printf("Fermeture du gestionnaire...\n");
+            break;
+        } else if (strcmp(command, "help") == 0) {
+            printf("Commandes : ls, mkdir <nom>, touch <nom> <contenu>, cd <nom>, read <nom>, rm <nom>, space, exit\n");
+        }else {
+            printf("Commande inconnue.\n");
+        }
+        
+    }
     return 0;
 }
